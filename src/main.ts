4b38159a12e8c753e4b9a2e9ce7bfd524ee80c99
ColMugX <colmugx@qq.com>
2019-03-18 23:53:36 +0800

    update: show current version and wanted version.

--- src/main.ts
+++ src/main.ts
@@ -14,15 +14,14 @@ export default async function() {
   const { stdout } = newSpawn(OUTDATED)
   const output = stdout.split('\n').slice(1)
   const apps = output
-    .map(v => v.match(/^(\S*)\s+/))
+    .map(v => v.match(/^(\S*\s+)(\S*)\s+(\S*)/))
     .filter(Boolean)
-    .map(v => v![0].trim())
+    .map(v => ({ name: `${v![1]} ${v![2]} ~> ${v![3]}`, value: v![1]}))
 
   if (!apps.length) {
     console.log('目前没有更新项')
     process.exit(0)
   }
-
   const answers = await inquirer.prompt({
     name: 'packages',
     message: "选择需要更新的包",
@@ -38,7 +37,7 @@ export default async function() {
 
   packages.forEach(pkg => {
     try {
-      const { stdout } = newSpawn(installPkg(pkg))
+      const { stdout } = newSpawn(installPkg(pkg.trim()))
       console.log(stdout.toString())
     } catch (err) {
       console.error(err)
@@ -47,5 +46,4 @@ export default async function() {
       console.log(`${pkg} 更新完成`)
     }
   });
-
 }
